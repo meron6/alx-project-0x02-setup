44405363024055cd14338ba68225c5a5b73b9cf0
meron6 <khozameron5@gmail.com>
2025-10-02 05:19:13 +0000

    update

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,4 +1,14 @@
+// Card Props Interface
 export interface CardProps {
   title: string;
+  description: string;
   content: string;
+}
+
+// Button Props Interface
+export interface ButtonProps {
+  label: string;
+  onClick: () => void;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
 }
